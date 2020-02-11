86e8f04795795eefe3a75f42819c6c9375ebbb5d
Gulshan Nayak <mr.gulshan.nayak@gmail.com>
2020-02-11 19:35:12 +0530

    git setup for service discovery

---
+++ Discovery-Service/src/test/java/com/gulshan/nayak/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulshan.nayak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
